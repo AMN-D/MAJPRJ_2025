1a3cc216b4bc60dcb7ad515d915749fab2224f49
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-10-10 12:07:43 +0530

    swiper function and div

--- Frontend/SAFER/home/static/home.js
+++ Frontend/SAFER/home/static/home.js
@@ -1,21 +1,12 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  loop:true,
+  loopedSlides: 5, //looped slides should be the same
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  observer: true, 
+  observeParents: true,
+  observeSlideChildren: true
 });
