7e5ec61bd709221926b462dd8e0d08c185900243
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-10-11 12:27:45 +0530

    Attempt of fixing android lag 2

--- Frontend/SAFER/home/static/home.js
+++ Frontend/SAFER/home/static/home.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper(".mySwiper", {
       loop: true, // Enable loop
-      lazy: true,
-      preloadImages: false,
       autoplay: {
           delay: 5000, // 5 seconds
           disableOnInteraction: false, // Keep autoplay after interaction
-      } // Transition speed in milliseconds (800ms = 0.8 seconds)
+      },
+      speed: 300, // Transition speed in milliseconds (800ms = 0.8 seconds)
   });
 });
 
