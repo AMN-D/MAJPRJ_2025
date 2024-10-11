d5135d5befb2ebff9d04a8ca09fde7ba389e893a
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-10-11 12:08:10 +0530

    fixed speed

--- Frontend/SAFER/home/static/home.js
+++ Frontend/SAFER/home/static/home.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
   var swiper = new Swiper(".mySwiper", {
       loop: true, // Enable loop
       autoplay: {
+          delay: 5000, // 5 seconds
           disableOnInteraction: false, // Keep autoplay after interaction
-      },
-      speed: 300, // Transition speed in milliseconds (800ms = 0.8 seconds)
+      } // Transition speed in milliseconds (800ms = 0.8 seconds)
   });
 });
 
