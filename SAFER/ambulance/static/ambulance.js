4fd99e2aedb90479fa954c3cc805f8685c84abad
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2025-01-24 08:54:13 +0530

    working location fetch
    - ip based location
    - gps tracking both working

--- SAFER/ambulance/static/ambulance.js
+++ SAFER/ambulance/static/ambulance.js
@@ -1,24 +1,54 @@
-var Latitude = document.getElementById("latitude").value;
-var Longitude = document.getElementById("longitude").value;
+let latitude, longitude, userLocationMarker, map;
 
-var map = L.map("map", {
-    center: [19.210095219278568, 73.15352289901084],
-    zoom: 14, 
-});
-mapLink = '<a href="http://www.esri.com/">Esri</a>';
-wholink = "SAFER";
-
-L.tileLayer(
-  "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/3/MapServer/tile/{z}/{y}/{x}",
-  {
-    attribution: "&copy; " + wholink + ", " + mapLink,
-    maxZoom: 18,
+function userLocation() {
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    initializeMap(latitude, longitude);
+    userLocationMarker = L.marker([latitude, longitude]).addTo(map);
+    userLocationMarker.bindPopup("<b>Your Location</b>").openPopup();
   }
-).addTo(map);
-
-L.tileLayer(
-  "https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}{r}.png",
-  {
-    opacity: 1,
+  function error() {
+    latitude = document.getElementById("latitude").value;
+    longitude = document.getElementById("longitude").value;
+    initializeMap(latitude, longitude);
+    var circle = L.circle([latitude, longitude], {
+      color: "blue",
+      fillColor: "#ADD8E6",
+      fillOpacity: 0.2,
+      radius: 500,
+    }).addTo(map);
+    circle.bindPopup("<b>Please allow location</b><br>for improve accuracy").openPopup();
   }
-).addTo(map);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+}
+
+function initializeMap(latitude, longitude) {
+  map = L.map("map", {
+    center: [latitude, longitude],
+    zoom: 18,
+  });
+  mapLink = '<a href="http://www.esri.com/">Esri</a>';
+  wholink = "SAFER";
+
+  L.tileLayer(
+    "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/3/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution: "&copy; " + wholink + ", " + mapLink,
+      maxZoom: 18,
+    }
+  ).addTo(map);
+
+  L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}{r}.png",
+    {
+      opacity: 1,
+    }
+  ).addTo(map);
+}
+
+window.addEventListener("load", userLocation);
