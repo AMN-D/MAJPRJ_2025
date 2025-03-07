ab0d3f83bfec2ea4a1f826bd4faf664a56599fbd
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2025-03-07 10:46:23 +0530

    pdf download working

---
+++ SAFER/disaster_assessment/static/disaster_assessment.js
@@ -0,0 +1,13 @@
+function downloadPDF() {
+  const element = document.documentElement;
+
+  const options = {
+    filename: "my-document.pdf",
+    margin: 1,
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
+
+  html2pdf().set(options).from(element).save();
+}
--- workflow.bat
+++ workflow.bat
@@ -9,3 +9,4 @@ start cmd /k sass --watch SAFER\disaster_assessment\static\disaster_assessment.s
 start cmd /k sass --watch SAFER\home\static\home.scss SAFER\home\static\home.css
 start cmd /k sass --watch SAFER\quick_first_aid\static\quick_first_aid.scss SAFER\quick_first_aid\static\quick_first_aid.css
 start cmd /k sass --watch SAFER\quick_first_aid\static\quick_first_aid_symptoms.scss SAFER\quick_first_aid\static\quick_first_aid_symptoms.css
+start cmd /k sass --watch SAFER\disaster_assessment\static\disaster_report.scss SAFER\disaster_assessment\static\disaster_report.css
