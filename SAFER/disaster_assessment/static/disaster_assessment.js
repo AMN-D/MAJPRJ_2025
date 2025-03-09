73985d43653aafc0768d37c64b191440f6e5e254
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2025-03-09 12:37:15 +0530

    fixed pdf ui being distrupted

--- SAFER/disaster_assessment/static/disaster_assessment.js
+++ SAFER/disaster_assessment/static/disaster_assessment.js
@@ -1,12 +1,3 @@
 function downloadPDF() {
-  const element = document.body; 
-  const options = {
-    filename: "my-document.pdf",
-    margin: 0, 
-    image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 2, useCORS: true }, 
-    jsPDF: { unit: "px", format: "a4", orientation: "portrait" }, 
-  };
-
-  html2pdf().set(options).from(element).save();
+  window.print();
 }
