23d651b1cacfeda520d98746da16127a42f5a1da
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2025-03-16 17:33:36 +0530

    speech recognition working but wihtout stemming

--- SAFER/quick_first_aid/views.py
+++ SAFER/quick_first_aid/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Symptom, QuickFirstAid
 
-# Create your views here.
+def select_symptoms(request):
+    symptoms = Symptom.objects.all()
+    diseases = None
+
+    if request.method == "POST":
+        selected_symptom_ids = request.POST.getlist("symptom")
+        if selected_symptom_ids:
+            diseases = QuickFirstAid.objects.filter(symptoms__id__in=selected_symptom_ids).distinct()
+
+    return render(request, "your_template.html", {"symptoms": symptoms, "diseases": diseases})
