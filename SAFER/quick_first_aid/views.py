f1be155ac6c1bfb55a06e9adb3764e9e031f6122
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2025-03-16 17:51:49 +0530

    nlp inplementation done in quick_first_aid

--- SAFER/quick_first_aid/views.py
+++ SAFER/quick_first_aid/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
-from .models import Symptom, QuickFirstAid
 
-def select_symptoms(request):
-    symptoms = Symptom.objects.all()
-    diseases = None
-
-    if request.method == "POST":
-        selected_symptom_ids = request.POST.getlist("symptom")
-        if selected_symptom_ids:
-            diseases = QuickFirstAid.objects.filter(symptoms__id__in=selected_symptom_ids).distinct()
-
-    return render(request, "your_template.html", {"symptoms": symptoms, "diseases": diseases})
+# Create your views here.
